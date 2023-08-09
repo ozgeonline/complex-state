12f80eed467e6ba613b1ce1c6dd93f4dd4bc14df
ozgeonline <nisroc.414@hotmail.com>
2023-08-09 11:48:06 +0300

    FName,LName,Email update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from "react"
 
 function App() {
+
+  const [contact,setContact] = useState({
+    fName:"",
+    lName:"",
+    email: ""
+  });
+  
+  function handleChange(event){
+    const {value,name} = event.target; //eventte setFullfName içinde erişme
+
+    setContact(prevValue => {
+      if(name === "fName"){
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email:prevValue.email
+        }
+      }else if(name === "lName"){
+        return{
+          lName:value,
+          fName:prevValue.fName,
+          email:prevValue.email
+        }
+      }else if(name === "email"){
+        return{
+          email:value,
+          fName:prevValue.fName,
+          lName:prevValue.lName
+        }
+      }
+    })
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello {contact.fName} {contact.lName}</h1>
+      <p>{contact.email}</p>
+      <form>
+        <input name="fName" onChange={handleChange} value={contact.fName} placeholder="First Name"/>
+        <input name="lName" onChange={handleChange} value={contact.lName} placeholder="Last Name"/>
+        <input name="email" onChange={handleChange} value={contact.email} placeholder="Email"/>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
